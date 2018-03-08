7e4ba8647bacf30ef0995243a3089ad5bf341e7c
Jonathan Ling <jonathan.ling@aptean.com>
2018-03-08 16:05:05 -0500

    Add axios and bodyparser.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+
+app.get('/:appName', function(req, res, next) {
+	let key = process.env[`${req.params.appName}_API_KEY`] || false;
+	if (!key) {
+		res.json({status: 'failed', response: `API key not found for ${req.params.appName}`});
+		return;
+	}
+	axios.get({
+
+	})
+	.then(function(response, err) {
+
+	});
+
+});
 
 app.get('*', function(req, res, next) {
 	res.json({name: 'api-forward', version: '1.0.0'});
