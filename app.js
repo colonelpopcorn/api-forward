43a3d40d25fc3db81de99bbe3812e6a2aa54f30e
Jonathan Ling <jonathan.ling@aptean.com>
2018-03-08 16:30:13 -0500

    Add cors for request forwarding

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const app = express();
-const axios = require('axios');
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const cors = requires('cors')
+
+app.use(cors())
 
 app.get('/:appName', function(req, res, next) {
-	let key = process.env[`${req.params.appName}_API_KEY`] || false;
+	let key = process.env[`${req.params.appName}_API_KEY`] || false
+
 	if (!key) {
-		res.json({status: 'failed', response: `API key not found for ${req.params.appName}`});
-		return;
+		res.json({status: 'failed', response: `API key not found for ${req.params.appName}`})
+		return
 	}
+
+	res.json({status: 'good', response: `GOOD!`})
+
+	/*
 	axios.get({
 
 	})
 	.then(function(response, err) {
 
-	});
-
-});
+	})
+	*/
+})
 
 app.get('*', function(req, res, next) {
-	res.json({name: 'api-forward', version: '1.0.0'});
-});
+	res.json({name: 'api-forward', version: '1.0.0'})
+})
 
-let portNumber = process.env.PORT || 3000;
+let portNumber = process.env.PORT || 3000
 
-app.listen(portNumber, () => { console.log(`Listening on port ${portNumber}!`); });
+app.listen(portNumber, () => { console.log(`Listening on port ${portNumber}!`) })
