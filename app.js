d628e7d7dfe57659acfc5659199b868780f1ad6e
Jonathan Ling <jonathan.ling@aptean.com>
2018-03-09 10:44:41 -0500

    Add axios function and config.

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+const app = require('express')()
 const axios = require('axios')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.use(cors())
+validateKey = function(req, res, next) {
+	req.apiKey = process.env[`${req.params.appName}_API_KEY`] || false
 
-app.get('/:appName', function(req, res, next) {
-	let key = process.env[`${req.params.appName}_API_KEY`] || false
+	req.apiUrl = process.env[`${req.params.appName}_URL`] || false
 
-	if (!key) {
+	if (!req.apiKey) {
 		res.json({status: 'failed', response: `API key not found for ${req.params.appName}`})
 		return
 	}
+	
+	if(!req.apiUrl) {
+		res.json({status: 'failed', response: `API url not found for ${req.params.appName}`})
+		return
+	}
 
-	res.json({status: 'good', response: `GOOD!`})
+	next()
+}
 
-	/*
-	axios.get({
-
+getResponse = function(req, res, next) {
+	let reqConfig = {
+		baseUrl: req.apiUrl,
+		params: req.body,
+	}
+	axios(reqConfig)
+	.then(data => {
+		res.json(data);
 	})
-	.then(function(response, err) {
-
+	.catch(error => {
+		console.log(error)
+		res.send({status: 'failed', response: `API request failed: ${error.message}`})
 	})
-	*/
-})
+}
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+app.get('/:appName', validateKey, getResponse)
 
 app.get('*', function(req, res, next) {
 	res.json({name: 'api-forward', version: '1.0.0'})
