f97462b391888014279554a76d104e2916b53e72
Jonathan Ling <jonathan.ling@aptean.com>
2018-08-06 08:41:10 -0400

    Start tesing some stuff!

--- src/Server/Server.spec.ts
+++ src/Server/Server.spec.ts
@@ -1,8 +1,15 @@
 import Server from "./Server"
 import * as assert from "assert";
 
-describe('Server class', function() {
-  describe('#bootstrap()', function() {
+describe('Server class', () => {
+  describe('#ctor()', () => {
+    it("Should have the right values when you construct it.", () => {
+      const TEST_PORT = "9000";
+      const TEST_DEFAULT_PORT = 8080;
+      let server = new Server(TEST_PORT, TEST_DEFAULT_PORT);
 
-  })
-})
+      assert.equal(server.PortNumber, 9000);
+      assert.equal(server.defaultPort, 8080);
+    })
+  });
+});
--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,9 +1,80 @@
 export default class Server {
-  public static bootstrap() : Server {
-    return new Server();
+
+  readonly PortNumber : number
+  private _defaultPort: number
+  get defaultPort() : number {
+    return this._defaultPort;
+  }
+  set defaultPort(newPort: number) {
+    this._defaultPort = newPort;
   }
 
-  constructor() {
+  constructor(portNumber: string, defaultPort: number) {
+    this.defaultPort = 8080; // Port defaults to 8080.
+    this.PortNumber = this.normalizePort(portNumber, this.defaultPort);
 
+  }
+   /**
+   * Normalize a port into a number, string, or false.
+   * @param val {string} The port string to normalize.
+   * @param defaultPort {number} The default port number to fall back to.
+   * @returns normalizedPort {number} The port number to pass to express.js.
+   */
+  public normalizePort(val : string, defaultPort : number) : number {
+    let port = parseInt(val, 0);
+
+    if (isNaN(port)) {
+      // named pipe
+      return port;
+    }
+
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+
+    return defaultPort;
+  }
+
+  /**
+   * Event listener for HTTP server "error" event.
+   */
+  public static getEventListeners() : object {
+    return {
+      onError(error : any, httpPort : number) {
+        if (error.syscall !== "listen") {
+          throw error;
+        }
+
+        var bind = typeof httpPort === "string"
+          ? "Pipe " + httpPort
+          : "Port " + httpPort;
+
+        // handle specific listen errors with friendly messages
+        switch (error.code) {
+          case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+          case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+          default:
+            throw error;
+        }
+      },
+
+      /**
+       * Event listener for HTTP server "listening" event.
+       */
+      onListening(httpServer : any, debug : Function) {
+        var addr = httpServer.address();
+        var bind = typeof addr === "string"
+          ? "pipe " + addr
+          : "port " + addr.port;
+        debug("Listening on " + bind);
+      }
+    }
   }
 }
